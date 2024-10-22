9c292b85896a8bcd11672de09040b4de1ad7b8a6
John <johnmciju712@gmail.com>
2024-10-22 10:42:54 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <section className=" h-dvh bg-slate-600">
+    <h1 className=" pt-9 text-center font-mono text-white text-4xl">Hello world</h1>
+   </section>
+  );
+}
